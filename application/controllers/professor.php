3221e621a52d5b0511cb4883d341ebceba882358
Cu-z <rodrigo.ross.comp@gmail.com>
2013-05-15 18:03:36 -0300

    Começo da implementação CRUD Pessoa e modificação CRUD Professor

--- application/controllers/professor.php
+++ application/controllers/professor.php
@@ -6,16 +6,7 @@ class Professor extends CI_Controller
         $this->load->model('professor_model');
         $this->load->helper('url_helper');
         $this->load->library(array('form_validation', 'session'));
-        $this->form_validation->set_rules('nome', 'Nome', 'required|max_length[255]');
         $this->form_validation->set_rules('email', 'E-mail', 'required|max_length[50]');
-        $this->form_validation->set_rules('dataNascimento', 'Data de Nascimento', 'required|max_length[10]');//ARRUMAR VALIDAÇÃO
-        $this->form_validation->set_rules('sexo', 'Sexo', 'validaSexo');
-        $this->form_validation->set_rules('cor', 'Cor', 'required|max_length[100]');//ARRUMAR COR NO BANCO DE DADOS PARA ENUMERADO
-        $this->form_validation->set_rules('escolaridade', 'Escolaridade', 'required|max_length[20]');//ARRUMAR NO BANCO DE DADOS PARA ENUMERADO
-        //$this->form_validation->set_rules('endereco_id', 'Endereco ID', 'required|max_length[11]');
-        //$this->form_validation->set_rules('graduacao', 'Graduação', 'required|max_length[100]');
-        $this->form_validation->set_rules('pessoa_id', 'Pessoa ID', 'required|max_length[100]');
-        
         $this->form_validation->set_message('required', 'O Campo %s é obrigatório!');
         
     }
@@ -59,12 +50,4 @@ class Professor extends CI_Controller
         }
     }
     
-    public function validaSexo($sexo)
-    {
-        if ($sexo == 'M' || $sexo == 'F' || $sexo == 'I'){
-            return TRUE;
-        } else {
-            return FALSE;
-        }
-    }
 }
