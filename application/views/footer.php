6b5d1a5e0bd9449bf5fc40145385ac3e153d142c
infocorp <presidencia.infocorp@gmail.com>
2013-05-16 18:02:02 -0300

    Alterado o rodape

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,7 @@
-    <footer class="navbar navbar-fixed-bottom">
-        RODAPE
+<footer id="footer" class="navbar navbar-fixed-bottom">
+        <div class="container">
+            <p class="muted credit"> Desenvolvido por <a href="http://infocorpconsultoria.com.br" target="_blank"> Infocorp</a> - Empresa Júnior do Instituto de Computação - UFMT</p>
+        </div>
     </footer>
     </body>
 </html>
